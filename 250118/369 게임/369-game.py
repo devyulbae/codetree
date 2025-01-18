0a07159d814736213996410189f877a1693ba671
Seongyul Bae <devyulbae@gmail.com>
2025-01-18 22:40:26 +0900

    Title: 369 게임, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250118/369 게임/369-game.py
+++ 250118/369 게임/369-game.py
@@ -1,7 +1,11 @@
 n = int(input())
 
-for i in range(1,n+1):
-    if(i%3==0) or ((i/10)%3==0) or (i%10 != 0 and (i%10)%3==0):
+for i in range(1,n+1,1):
+    if(i%3==0):
+        print(0,end=" ")
+    elif((i//10)==3 or (i//10)==6 or (i//10)==9):
+        print(0,end=" ")
+    elif((i%10)==3 or (i%10)==6 or (i%10)==9):
         print(0,end=" ")
     else:
-        print(i, end=" ")
+        print(i,end=" ")
