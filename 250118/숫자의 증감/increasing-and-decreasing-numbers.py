11e9227e8d70cb71056d455ee1ff280ed7d820b6
Seongyul Bae <devyulbae@gmail.com>
2025-01-18 21:41:16 +0900

    Title: 숫자의 증감, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

--- 250118/숫자의 증감/increasing-and-decreasing-numbers.py
+++ 250118/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -3,7 +3,7 @@ n = int(n)
 
 if(c=='A'):
     for i in range(1,n+1,1):
-        print(i)
+        print(i, end=" ")
 if(c=='D'):
     for i in range(n,0,-1):
-        print(i)
+        print(i, end=" ")
